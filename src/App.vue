6398ea27cfc9f9c1f2c1ca7d95795b5f48f6b037
Martin Z <mzenze@gmail.com>
2022-10-31 21:03:01 -0300

    Dynamic url bug repro

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
+<style>
+img {
+  max-width: 10rem;
+}
+</style>
+
 <template>
   <span>Hello Vite + vite-ssg</span>
+  <img :src="dynamicUrl" alt="img1" />
+  <img :src="staticUrl" alt="img2" />
 </template>
+
+<script setup lang="ts">
+const dynamicUrl = new URL('/dip.jpg', import.meta.url).href
+const staticUrl = '/dip.jpg'
+</script>
